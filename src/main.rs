a1aba37ad2a43d65d27304258e7615287bd2109f
Fernando Perry <fernandoperry1234@gmail.com>
2023-03-20 09:14:03 +0700

    added prompt_name to the config

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,43 @@
-use serde_derive::Deserialize;
 use core::panic;
+use rofi;
+use serde_derive::Deserialize;
 use std::env;
 use std::fs;
 use std::io::Write;
-use rofi; 
 
 #[derive(Deserialize)]
 struct Config {
+    prompt_name: String,
     pretty_name: Vec<String>,
     command: Vec<String>,
 }
 
-fn read_configuration() -> (Vec<String>, Vec<String>){
-    let home_folder: String = match env::var("HOME"){
+fn read_configuration() -> (Vec<String>, Vec<String>, String) {
+    let home_folder: String = match env::var("HOME") {
         Ok(val) => val,
         Err(err) => panic!("Error : Failed to read $HOME env var => {err}"),
     };
     let path_to_config: String = format!("{home_folder}/.config/cusprus.toml");
-        if std::path::Path::new(&path_to_config).is_file() == false {
-            let mut create_config = fs::File::create(&path_to_config).expect("Error : Failed to create the configuration file!");
-            create_config.write_all(b"pretty_name = [\"Example Name\", \"Example 2\"]\ncommand = [\"echo hello $USER\", \"echo this is example 2\"]").expect("Error : Failed to write the configuration file");
-            println!("Info : Created the configuration file.\nInfo : configuration file created at `{path_to_config}` with the example.");
+    if std::path::Path::new(&path_to_config).is_file() == false {
+        let mut create_config = fs::File::create(&path_to_config)
+            .expect("Error : Failed to create the configuration file!");
+        create_config.write_all(b"pretty_name = [\"Example Name\", \"Example 2\"]\ncommand = [\"echo hello $USER\", \"echo this is example 2\"]").expect("Error : Failed to write the configuration file");
+        println!("Info : Created the configuration file.\nInfo : configuration file created at `{path_to_config}` with the example.");
     }
-    let read_file = match fs::read_to_string(path_to_config){
+    let read_file = match fs::read_to_string(path_to_config) {
         Ok(val) => val,
         Err(err) => panic!("Error : Failed to read the configuration file => {err}"),
     };
-    let parse_file: Config = match toml::from_str(&read_file){
+    let parse_file: Config = match toml::from_str(&read_file) {
         Ok(val) => val,
         Err(err) => panic!("Error : Failed to parse the file => {err}"),
     };
-    return (
-        parse_file.pretty_name,
-        parse_file.command,
-            );
+    return (parse_file.pretty_name, parse_file.command, parse_file.prompt_name);
 }
 
-fn spawn_rofi(pretty_name: Vec<String>) -> usize {
+fn spawn_rofi(pretty_name: Vec<String>, prompt_name:String) -> usize {
     let selected = rofi::Rofi::new(&pretty_name)
-        .prompt("  Master ")   
+        .prompt(prompt_name)
         .run_index();
     let return_value = match &selected {
         Ok(val) => val,
@@ -49,10 +48,10 @@ fn spawn_rofi(pretty_name: Vec<String>) -> usize {
 
 fn main() {
     let configuration = read_configuration();
-    let value = spawn_rofi(configuration.0);
+    let value = spawn_rofi(configuration.0, configuration.2);
     std::process::Command::new("sh")
         .arg("-c")
-        .arg(format!("{}",configuration.1[value]))
+        .arg(format!("{}", configuration.1[value]))
         .spawn()
         .expect("Error : Failed to spawn the process");
 }
